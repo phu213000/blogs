6a6b0e087df41864b187f108e8d4196f9bf29a95
phu213000 <21089881.phu@student.iuh.edu.vn>
2024-11-20 16:27:32 +0700

    thêm trong cpanel : các đối tượng order , post, product

--- app/controllers/Product.php
+++ app/controllers/Product.php
@@ -4,8 +4,15 @@ class product extends Dcontroller{
     parent::__construct();
   }
 
-  public function chitietsanpham(){
-    echo "This is chitietsanpham ";
-  }
+ public function index(){
+  $this->add_product();
+ }
+
+ public function add_product(){
+  $this->load->view('cpanel/header');
+  $this->load->view('cpanel/menu');
+  $this->load->view('cpanel/product/add_product');
+  $this->load->view('cpanel/footer');
+ }
 }
 ?>
